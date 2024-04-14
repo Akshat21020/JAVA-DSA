f80f6d88a8a389e7e35c7f71b56ec2f0e6a9672d
Akshat21020 <166981782+Akshat21020@users.noreply.github.com>
2024-04-14 13:54:59 +0530

    Add files via upload

---
+++ simple.java
@@ -0,0 +1,11 @@
+package arrays;
+import java.util.*;
+public class simple {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i=1;i<=n;i++) {
+			System.out.println(i);
+		}
+	}
+}
