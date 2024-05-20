8506349ae04c81765e44b53fb781160bbf70f3cb
Akshat21020 <166981782+Akshat21020@users.noreply.github.com>
2024-05-20 23:15:59 +0530

    Update Check_Prime.java

--- DATA TYPES/Check_Prime.java
+++ DATA TYPES/Check_Prime.java
@@ -3,18 +3,24 @@ import java.util.*;
 public class Check_Prime {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc= new Scanner(System.in);
-		int n =sc.nextInt();
-		int count=0;
-		for(int i=2;i<=2;i++) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		if(n==1 || n==0) {
+			System.out.println("none");;
+		}
+		if(n>1) {
+		int count =0;
+		for(int i=2;i<n;i++) {
 			if(n%i==0) {
 				count++;
 			}
 		}
 		if(count>0) {
-			System.out.println("NOT PRIME");
+			System.out.println("not prime");
 		}else {
-			System.out.println("PRIME");
+			System.out.println("prime");
+		}
 		}
 	}
+
 }
